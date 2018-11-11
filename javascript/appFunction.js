a98c348280a54cdf45296eece2a0bf4398ebfa2a
Ismael Correa <ismaelcorrea@Ismaels-MacBook-Pro.local>
2018-11-11 13:51:18 -0600

    Update

---
+++ javascript/appFunction.js
@@ -0,0 +1,33 @@
+
+//On Click Events
+$(document).ready(function(){
+
+$("#homeValuation").on("click",function(){
+    alert("HOME Valuation Object");
+});
+
+$("#propertyDetails").on("click",function(){
+    alert("Property Details Object ");
+});
+
+$("#vacancy").on("click",function(){
+    alert("Vacancy Object");
+});
+
+$("#homeFlips").on("click",function(){
+    alert("Home Flips Object");
+});
+
+$("#interestRates").on("click",function(){
+    alert("Interest Rates Object");
+});
+
+$("#mortageRates").on("click",function(){
+    alert("Mortage Rates Object");
+});
+
+$("#postings").on("click",function(){
+    alert("Postings Object");
+});
+
+});
